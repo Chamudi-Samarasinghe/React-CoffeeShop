4de0f4a7c7defc4d794b9233276affbc809dbcef
Chamudi-Samarasinghe <ctsamarasinghe@students.nsbm.ac.lk>
2024-03-16 20:25:05 +0530

    Coffee Shop

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Latte from "../images/Latte.jpg";
+import BlackCoffee from "../images/Black-Coffee.jpg";
+import IrishCoffee from "../images/Irish-coffee.jpg";
+import Frappuccino from "../images/Frappuccino.jpg";
+import Doppio from "../images/Doppio.jpg";
+import Cappuccino from "../images/Cappuccino.jpg";
+
+export const MenuList = [
+  {
+    name: "Latte",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Latte,
+    price: 200,
+  },
+  {
+    name: "Black Coffee",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: BlackCoffee,
+    price: 250,
+  },
+  {
+    name: "Irish Coffee",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: IrishCoffee,
+    price: 300,
+  },
+  {
+    name: "Frappuccino",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Frappuccino,
+    price: 100,
+  },
+  {
+    name: "Doppio",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Doppio,
+    price: 400,
+  },
+  {
+    name: "Cappuccino",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Cappuccino,
+    price: 500,
+  },
+];
---
+++ src/pages/Contact.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Layout from "../components/Layout";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import MailIcon from "@mui/icons-material/Mail";
+import CallIcon from "@mui/icons-material/Call";
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <Box sx={{ my: 5, ml: 10, "& h4": { fontWeight: "bold", mb: 2 } }}>
+        <Typography variant="h4">Contact My Resturant</Typography>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem odio
+          beatae ducimus magni nobis culpa praesentium velit expedita quae,
+          corrupti, pariatur inventore laboriosam consectetur modi impedit
+          error, repudiandae obcaecati doloribus. Lorem ipsum dolor sit amet 
+          consectetur adipisicing elit. Commodi,deserunt libero reprehenderit 
+          cum sint fugit cumque temporibus modi facere eveniet amet obcaecati 
+          ducimus harum velit maxime vel qui voluptatibus quam odio corrupti saepe,
+           voluptas dolorum quidem tempore? Esse sapiente molestias minus enim quisquam 
+           dolorum eum culpa ullam impedit velit quo, corporis ducimus numquam dignissimos
+          inventore maiores.
+        </p>
+      </Box>
+      <Box
+        sx={{
+          m: 3,
+          width: "600px",
+          ml: 10,
+          "@media (max-width:600px)": {
+            width: "300px",
+          },
+        }}
+      >
+        <TableContainer component={Paper}>
+          <Table aria-label="contact table">
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  sx={{ bgcolor: "black", color: "white" }}
+                  align="center"
+                >
+                  Contact Details
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <SupportAgentIcon sx={{ color: "red", pt: 1 }} /> 1800-00-0000
+                  (tollfree)
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <MailIcon sx={{ color: "skyblue", pt: 1 }} /> help@myrest.com
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <CallIcon sx={{ color: "green", pt: 1 }} /> 1234567890
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Layout>
+  );
+};
+
+export default Contact;
